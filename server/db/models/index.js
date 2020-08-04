bb2a17da2687d1a39615706e01cea68f8d8c70c2
laurenpitruz <32465721+laurenpitruz@users.noreply.github.com>
2020-08-04 16:01:35 -0400

    Update index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -11,6 +11,9 @@ Order.belongsTo(User)
 Order.belongsToMany(Shoe, {through: 'purchased'})
 Shoe.belongsToMany(Order, {through: 'purchased'})
 
+Shoe.belongsTo(Brand)
+Brand.hasMany(Shoe)
+
 module.exports = {
   User,
   Shoe,
