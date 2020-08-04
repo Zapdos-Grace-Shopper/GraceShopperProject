350a78038669308dd38d8326e31657ff88160a16
Lauren <laurenpitruz@gmail.com>
2020-08-04 17:16:17 -0400

    converted navbar to react-bootstrap nav-bar and added basic css

--- client/app.js
+++ client/app.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import {Navbar} from './components'
+import ZapdosNavbar from './components/navbar'
 import Routes from './routes'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <ZapdosNavbar />
       <Routes />
     </div>
   )
