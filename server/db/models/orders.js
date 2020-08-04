aab4426a26104255e15c7db90bcf4cde8ec11d83
laurenpitruz <32465721+laurenpitruz@users.noreply.github.com>
2020-08-04 15:59:48 -0400

    Update orders.js

--- server/db/models/orders.js
+++ server/db/models/orders.js
@@ -3,7 +3,8 @@ const db = require('../db')
 
 const Order = db.define('order', {
   status: {
-    type: Sequelize.ENUM('complete')
+    type: Sequelize.ENUM('complete', 'incomplete'),
+    default: 'incomplete',
   }
 })
 
