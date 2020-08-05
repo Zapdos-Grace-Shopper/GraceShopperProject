458d2627e89f437c201c22c453d9a5b4939ab8a5
Olivia Kasmin <oliviakasmin@gmail.com>
2020-08-05 11:31:53 -0400

    wrote single shoe express routes in singleShoe temporary file

---
+++ server/api/singleShoe.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const {Shoe} = require('../db/models')
+module.exports = router
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const singleShoe = await Shoe.findByPk(req.params.id)
+    res.send(singleShoe)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const targetShoe = await Shoe.findByPk(req.params.id)
+    await targetShoe.update(req.body)
+    res.json(targetShoe)
+  } catch (err) {
+    next(err)
+  }
+})
