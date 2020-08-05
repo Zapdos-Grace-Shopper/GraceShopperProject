91847692f3fa04a7eb15fa8a802d75af4b975254
Irina Barreto <abraeva.98@icloud.com>
2020-08-05 18:51:57 -0400

    feat: added components

--- client/components/orders.js
+++ client/components/orders.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
 
-export const Orders = () => {
-  return <div>hello</div>
+export class Orders extends React.Component {
+  render() {
+    return <div>hello</div>
+  }
 }
