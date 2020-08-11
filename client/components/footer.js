093578a71d63f644a065243e1489e3b05f35b64b
Irina Barreto <abraeva.98@icloud.com>
2020-08-11 14:35:13 -0400

    feat

--- client/components/footer.js
+++ client/components/footer.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className="footer">
-      <h6 className="footerLeft">1234 Luxury Street</h6>
-      <h6 className="footerRight">Hello@Zapdos.com | 555-555-5555</h6>
-    </div>
+    <footer>
+      <div className="footer">
+        <h6 className="footerLeft">1234 Luxury Street</h6>
+        <h6 className="footerRight">Hello@Zapdos.com | 555-555-5555</h6>
+      </div>
+    </footer>
   )
 }
